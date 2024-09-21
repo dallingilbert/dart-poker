61faf502f788c323faf8ce8c7798b191aa2a83f4
dallingilbert <djgilb24@byui.edu>
2024-09-21 13:29:13 -0700

    Create player class

---
+++ src/player/Player.dart
@@ -0,0 +1,16 @@
+import '../card/Card.dart';
+
+class Player {
+  int funds;
+  String name;
+  Card ?card;
+
+  Player(this.funds, this.name) {
+    funds = this.funds;
+    name = this.name;
+  }
+
+
+
+  
+}
