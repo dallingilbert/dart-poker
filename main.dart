f1fcc01b43318d29d9ed8a8a98d9a78291368ddc
dallingilbert <djgilb24@byui.edu>
2024-09-23 15:01:17 -0700

    Rename card and player files to better fit Dart style standards.

--- main.dart
+++ main.dart
@@ -1,18 +1,18 @@
-import './src/player/Player.dart';
-import './src/card/Card.dart';
+import './src/player/player.dart';
+import 'src/card/card.dart';
 
 void main() {
   // Instantiate players
-  Player player1 = Player(1000, 'Saltyveins');
-  Player player2 = Player(2000, 'Salty');
+  Player player1 = Player(1000, 'Saltyveins', []);
+  Player player2 = Player(2000, 'Salty', []);
 
   // Create 3 random cards and add them to player 1
   for (int i = 0; i < 3; i++) {
     Card card = Card.random();
-    player1.cards!.add(card);
+    player1.cards.add(card);
   }
 
   // print player1 individual cards
-  player1.cards!.forEach((card) => print(card.cardNumber));
+  player1.cards.forEach((card) => print(card.cardNumber));
 
 }
