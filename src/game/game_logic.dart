864497bc0a3002d327fe13f40cc009ed29d5189f
dallingilbert <djgilb24@byui.edu>
2024-09-23 22:13:46 -0700

    Refactor GameLogic constructor and implement the dealer method

--- src/game/game_logic.dart
+++ src/game/game_logic.dart
@@ -6,19 +6,20 @@ class GameLogic {
   // Member variables
   List<Player> playerList;
   House house;
-  Card card;
   double bigBlindValue, smallBlindValue;
   int round, playerCount;
 
   // Constructor
-  GameLogic(this.playerList, this.house, this.card, this.bigBlindValue)
+  GameLogic(this.playerList, this.house, this.bigBlindValue)
     : smallBlindValue = bigBlindValue / 2,
       playerCount = playerList.length,
       round = 0;
 
   // Methods
   void initPlayerRoles() {
-    
+      playerList.first.isDealer = true;
+      for (int i = 1; i < playerList.length - 1; i++){
+      }
   }
 
   void dealCardsToPlayers() {
